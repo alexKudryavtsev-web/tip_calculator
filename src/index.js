9cd9692c89b9039c22d3e19e05ea4631d2ee3033
AlexanderKudryavtsev <qwertyshurazh@gmail.com>
2021-02-11 14:31:34 +0300

    empty project

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import reportWebVitals from './reportWebVitals'
+
+ReactDOM.render(
+  <div>
+    App
+  </div>,
+  document.getElementById('root')
+)
+
+reportWebVitals()
